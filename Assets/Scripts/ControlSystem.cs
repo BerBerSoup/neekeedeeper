cbc7c26b84e2e0af173cee7fb937fab790a80d1e
BerBerSoup <60650291+BerBerSoup@users.noreply.github.com>
2023-07-20 03:13:28 +0800

    角色控制

--- Assets/Scripts/ControlSystem.cs
+++ Assets/Scripts/ControlSystem.cs
@@ -3,10 +3,14 @@
 public class ControlSystem : MonoBehaviour
 {
     [Header ("移動速度")]
-    [Range (0,50)]
+    [Range (0,100)]
     public float moveSpeed = 10f;
     [Header("剛體")]
     public Rigidbody2D rig;
+    [Header("動畫控制器")]
+    public Animator ani;
+    [Header("跑步參數")]
+    public string parRun = "跑起來";
 
 
     private void Start()
@@ -28,7 +32,19 @@ public class ControlSystem : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        rig.velocity = new Vector2(h, v);
+        rig.velocity = new Vector2(h, v) * moveSpeed;
+
+        ani.SetBool(parRun, h != 0 || v != 0);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
     }
 
 }
